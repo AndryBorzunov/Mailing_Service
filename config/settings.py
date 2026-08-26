d2725395f95fc2969a9163d836cff5f5823388f9
Борзунов Андрей <andry73@yandex.ru>
2026-08-26 10:43:12 +0500

    test ci 18

--- config/settings.py
+++ config/settings.py
@@ -85,9 +85,9 @@ if os.getenv("GITHUB_ACTIONS") == "true":
         "default": {
             "ENGINE": "django.db.backends.postgresql",
             "NAME": "testdb",
-            "USER": "postgres",
-            "PASSWORD": "postgres",
-            "HOST": "localhost",          # важно: имя сервиса, не localhost
+            "USER": "andry",
+            "PASSWORD": "12345",
+            "HOST": os.getenv("HOST"),         # важно: имя сервиса, не localhost
             "PORT": "5432",
             # Явно запрещаем сокет и форсируем TCP
             "OPTIONS": {},
